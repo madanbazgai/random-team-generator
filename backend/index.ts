0f52a47421b5b7f1b13d0d509c085a7b5f1fdf7b
madanbazgai <madanbajgai@gmail.com>
2025-03-17 19:05:15 +0545

    db connection

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/team";
+
+if (!MONGODB_URI) {
+  console.error("MongoDB URI is not defined in the environment variables.");
+  process.exit(1);
+}
 
 app.use(cors());
 app.use(express.json());
@@ -20,10 +25,20 @@ app.use("/api", teamRoutes);
 app.use("/api", playerRoutes);
 app.use("/api", generateTeamRoutes);
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((error) => console.error("MongoDB connection error:", error));
+const connectWithRetry = () => {
+  console.log("MongoDB connection with retry");
+  mongoose
+    .connect(MONGODB_URI)
+    .then(() => {
+      console.log("Connected to MongoDB successfully");
+      app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
+      console.log("Retrying connection in 5 seconds...");
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+// Initial connection attempt
+connectWithRetry();
